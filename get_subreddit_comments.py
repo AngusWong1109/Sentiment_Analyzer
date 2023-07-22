d2b51eb18991c50ab133c457a7e69b821d71a639
lcchow <lcchow@sfu.ca>
2023-07-21 20:46:46 -0700

    add in files to get reddit data from cluster and set up city subreddit data

---
+++ get_subreddit_comments.py
@@ -0,0 +1,56 @@
+import sys
+from pyspark.sql import SparkSession, functions, types, Row
+
+spark = SparkSession.builder.appName('reddit extracter').getOrCreate()
+
+reddit_submissions_path = '/courses/datasets/reddit_submissions_repartitioned/'
+# reddit_comments_path = '/courses/datasets/reddit_comments_repartitioned/'
+reddit_comments_path_list = [
+    '/courses/datasets/reddit_comments_repartitioned/year=2015/*/*.json.gz',
+    '/courses/datasets/reddit_comments_repartitioned/year=2016/*/*.json.gz',
+    '/courses/datasets/reddit_comments_repartitioned/year=2017/*/*.json.gz',
+    '/courses/datasets/reddit_comments_repartitioned/year=2018/*/*.json.gz',
+    '/courses/datasets/reddit_comments_repartitioned/year=2019/*/*.json.gz',
+    '/courses/datasets/reddit_comments_repartitioned/year=2020/*/*.json.gz',
+    '/courses/datasets/reddit_comments_repartitioned/year=2021/*/*.json.gz',
+]
+
+output = 'reddit-subset'
+
+comments_schema = types.StructType([
+    types.StructField('archived', types.BooleanType()),
+    types.StructField('author', types.StringType()),
+    types.StructField('author_flair_css_class', types.StringType()),
+    types.StructField('author_flair_text', types.StringType()),
+    types.StructField('body', types.StringType()),
+    types.StructField('controversiality', types.LongType()),
+    types.StructField('created_utc', types.StringType()),
+    types.StructField('distinguished', types.StringType()),
+    types.StructField('downs', types.LongType()),
+    types.StructField('edited', types.StringType()),
+    types.StructField('gilded', types.LongType()),
+    types.StructField('id', types.StringType()),
+    types.StructField('link_id', types.StringType()),
+    types.StructField('name', types.StringType()),
+    types.StructField('parent_id', types.StringType()),
+    types.StructField('retrieved_on', types.LongType()),
+    types.StructField('score', types.LongType()),
+    types.StructField('score_hidden', types.BooleanType()),
+    types.StructField('subreddit', types.StringType()),
+    types.StructField('subreddit_id', types.StringType()),
+    types.StructField('ups', types.LongType()),
+    types.StructField('year', types.IntegerType()),
+    types.StructField('month', types.IntegerType()),
+])
+
+def main():
+    reddit_comments = spark.read.json(reddit_comments_path_list, schema=comments_schema)
+
+    subs = ['vancouver', 'toronto','Calgary', 'montreal', 'nyc', 'LosAngeles', 'boston', 'chicago', 'Seattle','Atlanta','sanfrancisco']
+
+    select_reddit_comments = reddit_comments.select(reddit_comments['subreddit'], reddit_comments['created_utc'], reddit_comments['body'])
+
+    select_reddit_comments.where(select_reddit_comments['subreddit'].isin(subs)) \
+        .write.json(output + '/comments', mode='overwrite', compression='gzip')
+
+main()
---
+++ setup_city_subreddits.py
@@ -0,0 +1,79 @@
+import sys
+from pyspark.sql import SparkSession, functions, types
+from datetime import datetime, timezone
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+spark = SparkSession.builder.appName('Analyze Reddit').getOrCreate()
+spark.sparkContext.setLogLevel('WARN')
+
+assert sys.version_info >= (3, 8) # make sure we have Python 3.8+
+assert spark.version >= '3.2' # make sure we have Spark 3.2+
+
+comments_schema = types.StructType([
+    types.StructField('subreddit', types.StringType()),
+    types.StructField('created_utc', types.StringType()),
+    types.StructField('body', types.StringType()),
+])
+
+file_path = 'reddit-subset/comments/part*'
+output = 'subreddit-output/subreddit-'
+sentimentAnalyzer = SentimentIntensityAnalyzer()
+
+def get_sentiment(text):
+    sentiment = sentimentAnalyzer.polarity_scores(text)
+    return sentiment_classification(sentiment['compound'])
+
+def sentiment_classification(score):
+    if score >= 0.05:
+        return "positive"
+    elif score > -0.05 and score < 0.05:
+        return "neutral"
+    else:
+        return "negative"
+
+def main():
+    data = spark.read.json(file_path, schema=comments_schema)
+
+    reddit_data = data.withColumn('timestamp', functions.from_unixtime(data['created_utc'],"yyyy-MM-dd"))
+    reddit_data = reddit_data.withColumn('year', functions.year(reddit_data['timestamp']))\
+                            .withColumn('month', functions.month(reddit_data['timestamp']))\
+                            .withColumn('day', functions.dayofmonth(reddit_data['timestamp']))
+
+    reddit_data = reddit_data.filter((reddit_data['body'] != "[deleted]") & (reddit_data['body'] != "[removed]"))\
+        .select(reddit_data['subreddit'], reddit_data['year'], reddit_data['month'],
+                reddit_data['day'], reddit_data['body'])
+
+    analyze_sentiment = functions.udf(get_sentiment, returnType=types.StringType())
+    reddit_data = reddit_data.withColumn('sentiment', analyze_sentiment(data['body']))
+
+    seattle_data = reddit_data.where(reddit_data['subreddit']=="Seattle")
+    losangeles_data = reddit_data.where(reddit_data['subreddit'] == "LosAngeles")
+    atlanta_data = reddit_data.where(reddit_data['subreddit'] == "Atlanta")
+    calgary_data = reddit_data.where(reddit_data['subreddit'] == "Calgary")
+    toronto_data = reddit_data.where(reddit_data['subreddit'] == "toronto")
+    montreal_data = reddit_data.where(reddit_data['subreddit'] == "montreal")
+    nyc_data = reddit_data.where(reddit_data['subreddit'] == "nyc")
+    boston_data = reddit_data.where(reddit_data['subreddit'] == "boston")
+    chicago_data = reddit_data.where(reddit_data['subreddit'] == "chicago")
+    vancouver_data = reddit_data.where(reddit_data['subreddit'] == "vancouver")
+    sanfran_data = reddit_data.where(reddit_data['subreddit'] == "sanfrancisco")
+
+    seattle_data.write.json(output+"seattle", compression='gzip', mode='overwrite')
+    losangeles_data.write.json(output + "la", compression='gzip', mode='overwrite')
+    atlanta_data.write.json(output + "atlanta", compression='gzip', mode='overwrite')
+    calgary_data.write.json(output + "calgary", compression='gzip', mode='overwrite')
+    toronto_data.write.json(output + "toronto", compression='gzip', mode='overwrite')
+    montreal_data.write.json(output + "montreal", compression='gzip', mode='overwrite')
+    nyc_data.write.json(output + "nyc", compression='gzip', mode='overwrite')
+    boston_data.write.json(output + "boston", compression='gzip', mode='overwrite')
+    chicago_data.write.json(output + "chicago", compression='gzip', mode='overwrite')
+    vancouver_data.write.json(output + "vancouver", compression='gzip', mode='overwrite')
+    sanfran_data.write.json(output + "sanfran", compression='gzip', mode='overwrite')
+
+
+    reddit_data.show()
+
+if __name__=='__main__':
+    # in_directory = sys.argv[1]
+    # out_directory = sys.argv[2]
+    main()
